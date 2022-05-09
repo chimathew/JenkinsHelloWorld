484dffd14237b1eda7f3927d8238139435167cd2
Chinnu Mathew <chinnumathew91@gmail.com>
2022-05-09 13:39:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package com.dodgeandcox.core.models;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("hello yo   u");
+		System.out.println("hello you");
 	}
 
 }
